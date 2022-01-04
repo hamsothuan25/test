6152d9780db9e9c507033dc0dd75b2a9f9faa5df
hamsothuan25 <76762965+hamsothuan25@users.noreply.github.com>
2022-01-04 12:20:00 +0700

    test

---
+++ app/src/main/java/com/example/lesson_1/vidu.kt
@@ -0,0 +1,10 @@
+package com.example.lesson_1
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
